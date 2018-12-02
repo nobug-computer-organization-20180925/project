bb5321eacc45d40054de09427ea4438fd2687092
luomin <dujl16@mails.tsinghua.edu.cn>
2018-12-02 23:46:47 +0800

    merged

--- id_ex.v
+++ id_ex.v
@@ -73,18 +73,25 @@ module id_ex(
 			ex_link_address <= `ZeroWord;
 			ex_is_in_delayslot <= `NotInDelaySlot;
 	    is_in_delayslot_o <= `NotInDelaySlot;			
-			
-		end else begin		
+		end else if(stall[2] == `Stop && stall[3] == `NoStop) begin
+			ex_aluop <= `EXE_NOP_OP;
+			ex_alusel <= `EXE_RES_NOP;
+			ex_reg1 <= `ZeroWord;
+			ex_reg2 <= `ZeroWord;
+			ex_wd <= `NOPRegAddr;
+			ex_wreg <= `WriteDisable;	
+			ex_link_address <= `ZeroWord;
+	    ex_is_in_delayslot <= `NotInDelaySlot;			
+		end else if(stall[2] == `NoStop) begin		
 			ex_aluop <= id_aluop;
 			ex_alusel <= id_alusel;
 			ex_reg1 <= id_reg1;
 			ex_reg2 <= id_reg2;
 			ex_wd <= id_wd;
-			ex_wreg <= id_wreg;	
+			ex_wreg <= id_wreg;		
 			ex_link_address <= id_link_address;
 			ex_is_in_delayslot <= id_is_in_delayslot;
-	    is_in_delayslot_o <= next_inst_in_delayslot_i;				
-			
+	    is_in_delayslot_o <= next_inst_in_delayslot_i;
 		end
 	end
 	
