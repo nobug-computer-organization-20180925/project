bbecc2e2e72cc970326fe5573d4033c266edbd9e
Unknown <dujl16@mails.tsinghua.edu.cn>
2018-12-03 22:18:12 +0800

    revert more

--- id_ex.v
+++ id_ex.v
@@ -24,7 +24,7 @@
 //////////////////////////////////////////////////////////////////////
 // Module:  id_ex
 // File:    id_ex.v
-// Description: ID/EX½×¶ÎµÄ¼Ä´æÆ÷
+// Description: ID/EXï¿½×¶ÎµÄ¼Ä´ï¿½ï¿½ï¿½
 // Revision: 1.0
 //////////////////////////////////////////////////////////////////////
 
@@ -36,7 +36,7 @@ module id_ex(
 	input wire rst,
 
 	
-	//´ÓÒëÂë½×¶Î´«µÝµÄÐÅÏ¢
+	//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½×¶Î´ï¿½ï¿½Ýµï¿½ï¿½ï¿½Ï¢
 	input wire[`AluOpBus]         id_aluop,
 	input wire[`AluSelBus]        id_alusel,
 	input wire[`RegBus]           id_reg1,
@@ -48,7 +48,7 @@ module id_ex(
 	input wire                    next_inst_in_delayslot_i,		
 	
 	
-	//´«µÝµ½Ö´ÐÐ½×¶ÎµÄÐÅÏ¢
+	//ï¿½ï¿½ï¿½Ýµï¿½Ö´ï¿½Ð½×¶Îµï¿½ï¿½ï¿½Ï¢
 	output reg[`AluOpBus]         ex_aluop,
 	output reg[`AluSelBus]        ex_alusel,
 	output reg[`RegBus]           ex_reg1,
@@ -72,25 +72,18 @@ module id_ex(
 			ex_link_address <= `ZeroWord;
 			ex_is_in_delayslot <= `NotInDelaySlot;
 	    is_in_delayslot_o <= `NotInDelaySlot;			
-		end else if(stall[2] == `Stop && stall[3] == `NoStop) begin
-			ex_aluop <= `EXE_NOP_OP;
-			ex_alusel <= `EXE_RES_NOP;
-			ex_reg1 <= `ZeroWord;
-			ex_reg2 <= `ZeroWord;
-			ex_wd <= `NOPRegAddr;
-			ex_wreg <= `WriteDisable;	
-			ex_link_address <= `ZeroWord;
-	    ex_is_in_delayslot <= `NotInDelaySlot;			
-		end else if(stall[2] == `NoStop) begin		
+			
+		end else begin		
 			ex_aluop <= id_aluop;
 			ex_alusel <= id_alusel;
 			ex_reg1 <= id_reg1;
 			ex_reg2 <= id_reg2;
 			ex_wd <= id_wd;
-			ex_wreg <= id_wreg;		
+			ex_wreg <= id_wreg;	
 			ex_link_address <= id_link_address;
 			ex_is_in_delayslot <= id_is_in_delayslot;
-	    is_in_delayslot_o <= next_inst_in_delayslot_i;
+	    is_in_delayslot_o <= next_inst_in_delayslot_i;				
+			
 		end
 	end
 	
